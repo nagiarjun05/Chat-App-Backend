f4f6e117e883b943a898860b7b71229388389a15
nagiarjun05 <nagiarjun05@gmail.com>
2023-01-02 19:03:22 +0530

    some rectification

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt=require('jsonwebtoken');
-const User=require('../models/User')
+const User=require('../models/user')
 const authentication=(req, res, next)=>{
     try{
         const token=req.headers.authorization;
